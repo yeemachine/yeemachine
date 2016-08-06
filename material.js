7bd224626301557072b11aa83f7d3a1b0049d78a
Richard Yee <richtomyee@gmail.com>
2016-08-06 09:14:56 -0700

    up

--- material.js
+++ material.js
@@ -28,6 +28,32 @@ function generateMorphTargets(mesh, geometry) {
 
 }
 
+function generateMorphTargets2(mesh, geometry) {
+
+  var vertices = [],
+    scale;
+
+  for (var i = 0; i < geometry.vertices.length; i++) {
+
+    vertices.push(geometry.vertices[i].clone());
+
+    scale = 1 + Math.random() * 1;
+
+    vertices[vertices.length - 1].x *= scale;
+    vertices[vertices.length - 1].y *= scale;
+    vertices[vertices.length - 1].z *= scale;
+
+  }
+
+  geometry.morphTargets.push({
+    name: "target1",
+    vertices: vertices
+  });
+
+  geometry.update
+
+}
+
 
 
 function updateMorphs(mesh, material) {
